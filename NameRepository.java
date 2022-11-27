d36700335cd9a1027ee634b67b062810f9b10a88
scottlee <113730997+scotttlee@users.noreply.github.com>
2022-11-27 15:12:16 -0800

    final commit

--- NameRepository.java
+++ NameRepository.java
@@ -1,32 +1,32 @@
 public class NameRepository implements Container {
-    
+
     public String names[] = {"Robert" , "John" ,"Julie" , "Lora"};
 
    @Override
-   public Iterator getIterator() {
+    public Iterator getIterator() {
       return new NameIterator();
-   }
+    }
 
-   private class NameIterator implements Iterator {
+    private class NameIterator implements Iterator {
 
-      int index;
+        int index;
 
-      @Override
-      public boolean hasNext() {
+        @Override
+        public boolean hasNext() {
       
-         if(index < names.length){
-            return true;
-         }
-         return false;
-      }
+            if(index < names.length){
+                return true;
+            }
+            return false;
+        }
 
-      @Override
-      public Object next() {
+        @Override
+        public Object next() {
       
-         if(this.hasNext()){
-            return names[index++];
-         }
-         return null;
-      }		
-   }
+            if(this.hasNext()){
+                return names[index++];
+            }
+            return null;
+        }		
+    }
 }
